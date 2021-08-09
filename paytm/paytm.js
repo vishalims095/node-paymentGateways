b259a07b48a6dd2f507e02c6caa79d72cc06887e
Vishal Sharma <vishal.sharma1@mail.vinove.com>
2021-08-09 21:14:34 +0530

    update

--- paytm/paytm.js
+++ paytm/paytm.js
@@ -5,4 +5,5 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 require('dotenv').config()
+
 app.listen(process.env.PORT,() => {console.log("Paytm Server listening at",process.env.PORT)})
