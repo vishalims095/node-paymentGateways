ee2183f7d7c9697cc37364ac87877da21bf7906b
vishal sharma <36037597+vishalims095@users.noreply.github.com>
2021-08-24 22:06:51 +0530

    Update script.js

--- paypal/script.js
+++ paypal/script.js
@@ -1,9 +1,9 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 const app = express()
-
-app.get('/',(req, res) =>{
-    res.sendFile(__dirname + "/index.html");
-})
+app.use(__dirname , "/index.html")
+// app.get('/',(req, res) =>{
+//     res.sendFile(__dirname + "/index.html");
+// })
 
 app.listen(3000,() => {console.log("paypal Server listening at 3000")})
